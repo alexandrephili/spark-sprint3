09bf102eb97a85d12f2366b41d8b11f587486130
Alexandre Philippe <alexandrephili@gmail.com>
2019-08-06 08:20:08 +0400

    Updates

---
+++ precache-manifest.65333f23823829becaf57c21f51b8373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb490f7a90e3a70fcaa35571afeb976",
+    "url": "/spark-sprint3/index.html"
+  },
+  {
+    "revision": "2d382921ce967b1cba98",
+    "url": "/spark-sprint3/static/css/main.d8bdb9a3.chunk.css"
+  },
+  {
+    "revision": "dfe7b6d276d4c81b108b",
+    "url": "/spark-sprint3/static/js/2.fec3726b.chunk.js"
+  },
+  {
+    "revision": "2d382921ce967b1cba98",
+    "url": "/spark-sprint3/static/js/main.c48fa6cc.chunk.js"
+  },
+  {
+    "revision": "a15655f90c73c75b5c7f",
+    "url": "/spark-sprint3/static/js/runtime~main.e9f9cae5.js"
+  }
+]);
